f3ae8a14830ca800e69b6971d55c2d08cc6e87a8
nico8156 <119879294+nico8156@users.noreply.github.com>
2026-01-20 21:52:41 +0100

    docs: restore README and gitignore; remove generated artifacts

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/nicolasmaldiney/ticketverify-engine
-# Build directory: /Users/nicolasmaldiney/ticketverify-engine/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/nlohmann_json-build")
-subdirs("tests")
--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/nicolasmaldiney/ticketverify-engine/tests
-# Build directory: /Users/nicolasmaldiney/ticketverify-engine/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/nicolasmaldiney/ticketverify-engine/build/tests/tv_tests-b12d07c_include.cmake")
-subdirs("../_deps/catch2-build")
